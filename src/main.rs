cf3a2716e84ef460b58393278f1ebcfcaa81aff8
FloatingComet62 <shubh2007rai@gmail.com>
2022-10-22 01:08:30 +0530

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
 #![no_std] // we don't want to include C standard library
 #![no_main] // since we don't include std, there is no main calling function
 
+mod vga_buffer;
 
 // Panic handler
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 // Main
 #[no_mangle] // don't change the function name, keep it _start
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello world{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
