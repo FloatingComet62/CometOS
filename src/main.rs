20407d70d8e0bc4d8b75656383ed87ae5a354ba6
FloatingComet62 <shubh2007rai@gmail.com>
2022-10-21 23:48:51 +0530

    A Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
 
 // Main
-#[no_mangle]
+#[no_mangle] // don't change the function name, keep it _start
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
